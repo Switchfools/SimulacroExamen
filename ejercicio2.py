6218810fcc9ad62ff88f517af5695bcd9756b5cc
switchfools-simulacroexamen-vt90i6zn <switchfools-simulacroexamen-vt90i6zn@local>
2018-05-24 17:36:59 +0000

    simulacro

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,11 @@
 # Escriba el codigo necesario para que al ejecutar python ejercicio2.py
 # se cree una grafica con una funcion sinusoidal entre 0 y 2pi.
+import matplotlib
+import numpy as np
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
+x=np.linspace(0,2*pi,100)
+y=np.sin(x)
+plt.plot(x,y)
+plt.show()
